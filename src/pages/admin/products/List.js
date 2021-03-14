06f894a14db03cd2ffa00787e29d7d3103f9ac51
datou-leo <2323178881@qq.com>
2021-03-14 08:18:16 +0800

    完成商品上下架功能

--- src/pages/admin/products/List.js
+++ src/pages/admin/products/List.js
@@ -1,11 +1,13 @@
 import  {React,useEffect,useState} from 'react'
 import {Card,Table,Button,Popconfirm} from 'antd'
-import {listApi,delOne} from "../../../services/products";
+import {listApi, delOne, modifyOne} from "../../../services/products";
+import "./List.css"
 
 function List(props){
 
     const [dataSource,setDataSource]=useState([]);
     const [total,setTotal]=useState(0);
+    const [currentPage,setCurrentPage]=useState(1);
 
     useEffect(()=>{
         listApi().then(res=>{
@@ -18,6 +20,7 @@ function List(props){
 
     const loadData=(page)=>{
         console.log(page);
+        setCurrentPage(page);
         listApi(page).then(res=>{
             console.log(res);
             setDataSource(res.products);
@@ -45,6 +48,10 @@ function List(props){
         title:'价格',
         dataIndex:'price'
     },{
+        title:'在售',
+        dataIndex:'onsale',
+        render:(txt,record,index)=>(record.onsale==1)?"在售":"下架"
+    },{
         title:'操作',
         render:(txt,record,index)=>{
             return (
@@ -54,12 +61,19 @@ function List(props){
                     }}>修改</Button>
                     <Popconfirm title="确定要删除此项么?" onCancel={()=>{console.log("用户取消删除")}} onConfirm={()=>{
                         delOne(record.id).then(res=>{
-                            loadData(1);
+                            loadData(currentPage);
                         });
                         console.log("用户确认删除")
                     }}>
                     <Button type="danger" style={{margin:"0 1rem"}} size="small">删除</Button>
                     </Popconfirm>
+                    <Button type="primary" size="small" onClick={()=>{
+                        modifyOne(record.id,{onsale:(record.onsale==1?0:1)}).then(res=>{
+                            loadData(currentPage);
+                        }).catch(err=>{
+                            console.log(err);
+                        });
+                    }}>{(record.onsale==1)?"下架":"上架"}</Button>
                 </div>
             )
         }
@@ -74,7 +88,7 @@ function List(props){
         }
         >
 
-            <Table rowKey="id" pagination={{total,defaultPageSize:3,onChange:loadData}} columns={columns} bordered dataSource={dataSource}/>
+            <Table  rowClassName={record =>(record.onsale==1)?"":"bg-red"} rowKey="id" pagination={{total,defaultPageSize:3,onChange:loadData}} columns={columns} bordered dataSource={dataSource}/>
         </Card>
     )
 
